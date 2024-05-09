5a6a686fdf9ae7c534a18c09ebfb160af66772c8
abdoulaye-ndiaye <wizlaye7@gmail.com>
2024-05-09 13:23:13 +0000

    modif sergin si

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/contact', function () {
     return view('contact');
 });
 Route::resource('users', UserController::class);
-Auth::routes();
+//Auth::routes();
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
