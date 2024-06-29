c893c6a0f523b95ceaebee333d072e8b30b6308e
Ayoba <lamineayoba@gmail.com>
2024-06-29 16:25:34 +0000

    update-index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -291,9 +291,9 @@
 
             <div class="section-title">
                 <h2>Actus</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+                {{-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
                     consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-                    fugiat sit in iste officiis commodi quidem hic quas.</p>
+                    fugiat sit in iste officiis commodi quidem hic quas.</p> --}}
             </div>
 
             <div class="row">
